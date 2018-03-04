649cd2db8c25780b07abeb959ca6ccfe9860be35
DanEscudero <daniel.escudero05@gmail.com>
2018-03-04 18:21:07 -0300

    done

--- lista3/ex1.c
+++ lista3/ex1.c
@@ -24,7 +24,7 @@ void freeList (node *lst)
 	}
 }
 
-/* Cria nó com dado x depois da posição i	 	*
+/* Cria nó com dado x seguinte a posição i	 	*
  * Assume que a lista tem tamanho i, no minimo 	*/
 void insere (node *lst, int x, int i)
 {
@@ -37,7 +37,7 @@ void insere (node *lst, int x, int i)
 	curr->next = novo;
 }
 
-/* Remove o elemento depois de i */
+/* Remove o elemento seguinte a posicao i */
 void removeNode (node *lst, int i)
 {
 	node *curr;
@@ -78,9 +78,50 @@ node * split (node *lst)
 		aux = aux->next;
 		meio = meio->next;
 	}
+	
 	aux->next = NULL;
 	
 	return meio;
+}
+
+void printCirc (node *lst)
+{
+	node *aux = lst;
+	printf ("%d ", aux->data);	
+	
+	aux = lst->next;
+	
+	while (aux != lst) {
+		printf ("%d ", aux->data);
+		aux = aux->next;
+	}
+	
+	printf ("\n");
+}
+
+void appendCirc(node *lst, int x)
+{
+	node *last = lst->next;
+	
+	while (last->next != lst)
+		last = last->next;
+	
+	node *new = malloc(sizeof(node));
+	new->data = x;
+	new->next = last->next;
+	
+	last->next = new;	
+}
+
+void removeLastCirc(node *lst)
+{
+	node *penultimo = lst->next;
+	while (penultimo->next->next != lst)
+		penultimo = penultimo->next;
+	
+	node *temp = penultimo->next;
+	penultimo->next = penultimo->next->next;
+	free(temp);	
 }
 
 int main ()
@@ -101,27 +142,63 @@ int main ()
 	
 	printList(lst);
 	
-	insere (lst, 111, 0);
+	printf("===\n");
+	
+	insere (lst, 99, 0);
 	printList(lst);
 	
 	removeNode (lst, 1);
 	printList(lst);
-	
+		
 	reverse(&lst);
 	printList(lst);
 
-	reverse(&lst);	
+	printf("===\n");
+	
+	reverse(&lst);
 	removeNode(lst, 0);
 	insere (lst, 1, 0);
 	insere (lst, 3, 2);
 	insere (lst, 4, 3);
 	insere (lst, 5, 4);
 	printList(lst);
-	
-	printf ("metade:\n");
+
+	printf("===\n");
+	printf("metade:\n");
 	metade = split(lst);
 	printList(lst);
-	printList(metade);	
+	printList(metade);
+	
+	freeList(lst);
+	freeList(metade);
+	
+	printf("===\n");
+	printf("Circular:\n");
+	
+	node *circular = malloc(sizeof(node));
+	node *circular2 = malloc(sizeof(node));
+	node *circular3 = malloc(sizeof(node));
+	
+	circular->data = 0;
+	circular->next = circular2;
+	circular2->data = 1;
+	circular2->next = circular3;
+	circular3->data = 2;
+	circular3->next = circular;
+	
+	printCirc(circular);
+	appendCirc(circular, 33);
+	printCirc(circular);
+	removeLastCirc(circular);
+	printCirc(circular);
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
