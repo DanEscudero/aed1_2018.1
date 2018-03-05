80304f95cdbaccb6f57d07cae8578c4c93b64065
DanEscudero <daniel.escudero05@gmail.com>
2018-03-05 11:26:56 -0300

    3 bugado

--- lista2/ex3.c
+++ lista2/ex3.c
@@ -1,20 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void divide (int *vet, int *pos, int *neg, int n, int *len_pos, int *len_neg) {
-	pos = malloc(sizeof(int));
-	neg = malloc(sizeof(int));
+void divide (int *vet, int **pos, int **neg, int n, int *len_pos, int *len_neg) {
+	printf("divide\n");
+	*pos = malloc(sizeof(int));
+	*neg = malloc(sizeof(int));
+	printf("aloca\n");
+	
 	for (int i = 0; i < n; i++) {
+		printf("for\n");
 		if (vet[i] > 0) {
-			//printf ("DONE %d\n", i);
-			pos = realloc(pos, sizeof(int) * ++(*len_pos));
-			pos[*(len_pos)-1] = vet[i];
+			printf ("DONE +\n");
+			
+			*pos = realloc(*pos, sizeof(int) * ++(*len_pos));
+			*pos[*(len_pos)-1] = vet[i];
 		}
 		else if (vet[i] < 0) {
-			neg = realloc(neg, sizeof(int) * ++(*len_neg));
-			neg[*(len_neg)-1] = vet[i];
+			printf ("DONE -\n");
+			
+			*neg = realloc(*neg, sizeof(int) * ++(*len_neg));
+			*neg[*(len_neg)-1] = vet[i];
+			
+			printf("fim if %d\n", *len_neg);
 		}
-		//vet = realloc(vet, sizeof(int) * --n);
 	}
 	free(vet);
 }
@@ -28,7 +36,8 @@ void printVet (int *vet, int n) {
 int main ()
 {
 	int *vet = malloc (sizeof(int) * 15);
-	int *pos, *neg;
+	int *pos = malloc(sizeof(int));
+	int *neg = malloc(sizeof(int));
 	int len_pos = 0, len_neg = 0;
 	
 	for (int i = 0; i < 15; i++)
@@ -37,7 +46,7 @@ int main ()
 	printVet(vet, 15);
 	printf ("===\n");
 	
-	divide(vet, pos, neg, 15, &len_pos, &len_neg);
+	divide(vet, &pos, &neg, 15, &len_pos, &len_neg);
 	
 	printVet(pos, len_pos);
 	printf ("===\n");
