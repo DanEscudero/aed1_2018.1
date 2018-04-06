13ba86a2e1f9f7a4091001fa900099327ec3cadc
DanEscudero <daniel.escudero05@gmail.com>
2018-04-06 16:04:22 -0300

    insertElement() missing

--- desafio.c
+++ desafio.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Celula, tem o indice e os valores, e aponta para as proximas celulas */
+/* Celula tem o indice e os valores e aponta para as proximas celulas */
 typedef struct node
 {
 	int data;
@@ -11,147 +11,146 @@ typedef struct node
 	struct node *right;
 } node;
 
-/* Borda, aponta para a celula e tem o indice */
-typedef struct border
-{
-	int index;
-	struct border *next;
-	node *cell;
-} border;
-
-/* Matriz, aponta para as bordas */
+/* Matriz aponta para as celulas de borda */
 typedef struct matriz
 {
-	border *lins;
-	border *cols;
+	int nLins;
+	int nCols;
+	node *lins;
+	node *cols;
 } matriz;
 
-void printBorders(matriz *m)
+/* Seta a matriz com l, c e os pointeiros NULL */
+matriz *setupMatriz(int l, int c)
 {
-	border *aux = m->lins;
-	printf("lins: ");
-	while (aux) {
-		printf("%d ", aux->index);
-		aux = aux->next;
+	matriz *m = malloc(sizeof(matriz));
+	
+	if (!m) return m;
+	
+	m->nLins = l;
+	m->nCols = c;
+	m->lins = NULL;
+	m->cols = NULL;
+	
+	return m;
+}
+
+/* Acessa a posicao (x,y) de m e retorna o elemento caso exista, 0 caso nao exista */
+int acesso (matriz *m, int y, int x)
+{
+	node *aux_lin = m->lins;
+	node *aux_col = m->cols;
+	
+	int line_counter = 0;
+	
+	if (!aux_lin || !aux_col) return 0;
+	
+	while (aux_lin->lin < y && aux_lin->down) {
+		aux_lin = aux_lin->down;
+		line_counter++;
 	}
 	
-	aux = m->cols;
-	printf("\ncols: ");
-	while (aux) {
-		printf("%d ", aux->index);
-		aux = aux->next;
+	while (aux_col->col < x && aux_col->right) {
+		aux_col = aux_col->right;
+	}
+	
+	if (aux_col->col == x && aux_lin->lin == y) {
+		for (int i = 0; i < line_counter; i++) {
+			aux_col = aux_col->down;
+		}
+		return aux_col->data;
+	}
+	else
+		return 0;
+}
+
+
+
+/* Imprime a m formatada */
+void printMatriz (matriz *m)
+{
+	for (int i = 0; i < m->nLins; i++) {
+		printf("[");
+		
+		for (int j = 0; j < m->nCols; j++)
+			printf("%d ", acesso(m, i, j));
+		
+		printf("]\n");
 	}
 	printf("\n");
 }
 
-/* INUTIL */
-/* Insere os nos de borda, com os paramtros (-1, NULL, NULL) */
-border * insertBorder (border *inicio)
+void multiplica (matriz *m, matriz *a, matriz *b)
 {
-	border *novo = malloc(sizeof(border));
-	novo->index = -1;
-	novo->next = NULL;
-	novo->cell = NULL;
-	
-	if (!inicio)
-		return novo;
-	
-	border *aux = inicio;
-	while (aux->next)
-		aux = aux->next;
-	
-	aux->next = novo;
-	return inicio;
-}
-
-/* Verifica se x pertence a lista inicio (ordenada) 			*
- * Retorna NULL caso nao pertenca, e o endereco caso pertenca 	*/
-border *pertence(border * inicio, int x)
-{
-	border *aux = inicio
-	while (aux && aux->index <= x) {
-		if (x == aux->indice) return aux;
-		aux = aux->next;
-	}
-	return NULL;
-}
-
-void insertElement (matriz* m, int lin, int col, int x)
-{
-	cell *novo = malloc(sizeof(cell));
-	novo->data = x;
-	novo->lin = lin;
-	novo->col = col;
-	novo->down = NULL;
-	novo->right = NULL;
-	
-	border *pertence_lin = pertence(m->lins, lin);
-	border *pertence_col = pertence(m->cols, col);
-	
-	//Checar se ja existe elemento naquela posicao
-	if (pertence_lin && pertence_col) {
-		//atualiza
-	}
-	//Nao existe linha e nem cluna do elemento
-	else if (!pertence_lin && !pertence_col) {
-		//cria lin e col
-	}
-	else if (pertence_lin && !pertence_col) {
-		//cria col
-	}
-	else if (!pertence_lin && pertence_col) {
-		//cria lin
+	for (int i = 0; i < a->nLins; i++) {
+		for (int j = 0; j < b->nCols; j++) {
+			int soma = 0;
+			
+			for (int k = 0; k < b->nLins; k++)
+				soma += acesso(a, i, k)*acesso(b, k, j);
+			
+			//insertElement(sum, i, j);
+		}
 	}
 }
 
 int main ()
 {
-	matriz *matrizA = malloc(sizeof(matriz));
-	matriz *matrizB = malloc(sizeof(matriz));
-	
-	if (!(matrizA && matrizB)) return 1;
-	
-	matrizA->lins = NULL;
-	matrizA->cols = NULL;
-
-	matrizB->lins = NULL;
-	matrizB->cols = NULL;
-	
-	//input dos parameetros das matrizes
+	//input dos parametros das matrizes
 	int la, ca, na, lb, cb, nb;
 	scanf("%d %d %d %d %d %d", &la, &ca, &na, &lb, &cb, &nb);
 	
-	//input de cada elemento
+	//Setup das matrizes
+	matriz *matrizA = setupMatriz(la, ca);
+	matriz *matrizB = setupMatriz(lb, cb);
+	
+	if (!(matrizA && matrizB)) return 1;
+		
+	
+	//input de elementos
 	int lin, col, num;
 	
-	/*printf("INSERINDO BORDAS\n");
-	for (int i = 0; i < la; i++)
-		matrizA->lins = insertBorder(matrizA->lins);
-	
-	for (int i = 0; i < ca; i++)
-		matrizA->cols = insertBorder(matrizA->cols);
-
-	for (int i = 0; i < lb; i++)
-		matrizB->lins = insertBorder(matrizB->lins);
-	
-	for (int i = 0; i < cb; i++)
-		matrizB->cols = insertBorder(matrizB->cols);
-	printf("BORDAS INSERIDAS\n");*/
-	
-	printBorders(matrizA);
-	printBorders(matrizB);
-	
-	printf("INSERINDO EM A\n")
-	for (int i = 0; i < na; i++) {
+	/*for (int i = 0; i < na; i++) {
 		scanf("%d %d %d", &lin, &col, &num);
 		insertElement(matrizA, lin, col, num);
 	}
 	
-	printf("INSERINDO EM B\n")
 	for (int i = 0; i < na; i++) {
 		scanf("%d %d %d", &lin, &col, &num);
 		insertElement(matrizB, lin, col, num);
-	}
+	}*/
+	
+	printf("MATRIZ A:\n");
+	printMatriz(matrizA);
+	printf("MATRIZ B:\n");
+	printMatriz(matrizB);
+	
+	
+	/*char input;
+	scanf("%c", &input);
+	while (!(input == 's' || input == 'S')) {
+		switch (input) {
+			case 'A':
+			case 'a':
+				printMatriz(matrizA);
+				break;
+			case 'B':
+			case 'b':
+				printMatriz(matrizB);
+				break;
+			case 'M':
+			case 'm':
+				if (ca != lb) printf("ERRO\n");
+				else {
+					matriz *mult = setupMatriz(ca, lb);
+					multiplica(mult, matrizA, matrizB);
+					printMatriz(mult);
+				}
+				break;
+		}
+		scanf("%c", &input);
+	}*/
+	
 	
 	return 0;
 }
