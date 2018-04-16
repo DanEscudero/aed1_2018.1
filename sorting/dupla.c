27c42a7793c939fe4368b02537a22d273c4d0cea
DanEscudero <daniel.escudero05@gmail.com>
2018-04-16 19:08:20 -0300

    selection pronto, collabedit.com/535jr

--- sorting/dupla.c
+++ sorting/dupla.c
@@ -3,79 +3,13 @@
 #include <string.h>
 
 typedef struct Lista {
-	int RA;
 	char *nome;
+	int nota;
+	int ra;
 	struct Lista *next;
 } lista;
 
-/* param: 1 = ra, 2 = nome */
-lista *min_List (lista *lst, int param)
-{
-	if (!lst) return NULL;
-	lista *aux = lst;
-	lista *min;
-	if (param == 1) {
-		min = aux;
-		while (aux) {
-			if (aux->RA < min->RA) min = aux;
-			aux = aux->next;
-		}
-	}
-	return min;
-}
-
-/* param: 1 = ra, 2 = nome */
-/*int *selection_sort (lista *lst, int param)
-{
-	int comparacoes = 0;
-	lista *aux = lst;
-	while (aux) {
-		aux = aux->next;
-	}
-}*/
-
-/* coloca no com nome e ra no fim da lista lst e retorna */
-lista* appendList (lista *lst, char *nome, int ra)
-{
-	lista *novo = malloc(sizeof(lista));
-	novo->nome = nome;
-	novo->RA = ra;
-	novo->next = NULL;
-	
-	if (!lst) return novo;
-	else {
-		lista *aux = lst;
-		while (aux->next)
-			aux = aux->next;
-		aux->next = novo;
-	}
-	return lst;
-}
-
-/*int selection_sort (int *v, int n)
-{
-	int comparacoes = 0;
-	for (int i = 0; i < n-1; i++) {
-		int min = i;
-		for (int j = i+1; j < n; j++) {
-			if (v[min] > v[j])
-				min = j;
-			comparacoes++;
-		}
-		swap(&v[i], &v[min]);
-	}
-	return comparacoes;
-}*/
-
-void printList(lista *lst)
-{
-	lista *aux = lst;
-	while (aux) {
-		printf("Nome: %s RA: %d\n", aux->nome, aux->RA);
-		aux = aux->next;
-	}
-}
-
+/* Libera lst e retorna NULL */
 lista* liberar (lista *lst){
     if (lst) {
         lst->next = liberar(lst->next);
@@ -84,33 +18,133 @@ lista* liberar (lista *lst){
 	return NULL;
 }
 
-int buscaList (lista* lst, int ra)
+/* Remove o no que contem ra */
+lista *removeList (lista *lst, int ra)
+{
+	lista *aux = lst;
+	if (aux->ra == ra) {
+		free(aux);
+		return lst->next;
+	}
+	while (aux->next) {
+		if (aux->next->ra == ra) {
+			lista *tmp = aux->next;
+			
+			if (!aux->next->next)
+				aux->next = NULL;
+			
+			else
+				aux->next = aux->next->next;
+			
+			free(tmp);
+			return lst;
+		}
+		aux = aux->next;
+	}
+	return lst;
+}
+
+/* Retorna o indice de ra presente em lst, -1 caso nao encontre */
+int buscaLista_ra (lista* lst, int ra)
 {
 	int i = 0;
 	lista *aux = lst;
 	while (aux) {
-		i++;
-		printf("aux->ra: %d\n", aux->RA);
-		if (aux->RA == ra)
+		if (aux->ra == ra)
 			return i;
+		i++;
 		aux = aux->next;
 	}
 	return -1;
 }
 
+/* Retorna o menor no da lista lst, baseado em param */
+/* param: 1 = ra, 2 = nome */
+lista *minList (lista *lst, int param, int *comparacoes)
+{
+	if (!lst) return NULL;
+	
+	lista *aux = lst;
+	lista *min = aux;
+	
+	while (aux->next) {
+		(*comparacoes)++;
+		if ((param == 1 && aux->ra < min->ra) || 				//MINIMO POR RA
+			(param == 2 && strcmp(aux->nome, min->nome) < 0))	//MINIMO POR NOME
+				min = aux;
+		aux = aux->next;
+	}
+	return min;
+}
+
+/* Coloca no com nome e ra no fim da lista lst e retorna a nova lista */
+lista* appendList (lista *lst, char *nome, int ra, int nota)
+{	
+	lista *novo = malloc(sizeof(lista));
+	novo->nome = nome;
+	novo->ra = ra;
+	novo->nota = nota;
+	novo->next = NULL;
+	
+	if (!lst) return novo;
+	
+	else {
+		lista *aux = lst;
+		while (aux->next)
+			aux = aux->next;
+		aux->next = novo;
+	}
+	
+	return lst;
+}
+
+void printList(lista *lst)
+{
+	printf("LISTA:\n");
+	lista *aux = lst;
+	while (aux) {
+		printf("Nome: %s ra: %d nota: %d\n", aux->nome, aux->ra, aux->nota);
+		aux = aux->next;
+	}
+}
+
+/* ta pronta ESTA MERDA */
+/* Recebe uma lista a ser ordenada, um parametro e o ponteiro para comparacoes */
+/* Retorna a lista ordenada */
+/* param: 1 = ra, 2 = nome */
+lista *selection_sort (lista *lst, int param, int *comparacoes)
+{
+	lista *sorted = NULL;
+	lista *min = NULL;
+	
+	while (lst) {
+		min = minList(lst, param, comparacoes);
+		sorted = appendList(sorted, min->nome, min->ra, min->nota);
+		lst = removeList(lst, min->ra);
+	}
+	lst = liberar(lst);
+	return sorted;
+}
+
 int main ()
 {
 	lista *lst = NULL;
-	char *teste = "AAA";
+	char *teste = "aaa";
+	int comparacoes;
 	
-	for (int i = 0; i < 5; i++) {
-		lst = appendList(lst, teste, 111*i);
-	}
+	lst = appendList(lst, "aaa", 10, 10);
+	lst = appendList(lst, "ccc", 18, 18);
+	lst = appendList(lst, "bbb", 15, 15);
+	lst = appendList(lst, "ddd", 60, 60);
 	printList(lst);
-	lst = liberar(lst);
-	printList(lst);
+	printf("\n");
 	
-	printf("%d\n", buscaList(lst, 333));
+	lst = selection_sort(lst, 1, &comparacoes);
+	printf("\nSORTED ");
+	printList(lst);
+	printf("C=%d\n", comparacoes);
+	
+	liberar(lst);
 	
 	return 0;
 }
