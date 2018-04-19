8d954dc4001cf883ca585aa2fc80f246ab8ac4f0
DanEscudero <daniel.escudero05@gmail.com>
2018-04-18 22:26:01 -0300

    Selection ok, falta insertion

--- sorting/dupla.c
+++ sorting/dupla.c
@@ -1,194 +1,222 @@
 /* DUPLA:  11005716 Daniel Escudero
-           -------- Caio Cesar      */
+           11016916 Caio Cesar      */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct Lista {
-    char *nome;
+    char nome[51];
     int nota;
     int ra;
     struct Lista *next;
-} lista;
+}lista;
 
-/* Coloca no com nome e ra no fim da lista lst e retorna a nova lista */
-lista* appendList (lista *lst, char *nome, int ra, int nota)
-{   
-    lista *novo = malloc(sizeof(lista));
-    novo->nome = nome;
-    novo->ra = ra;
-    novo->nota = nota;
-    novo->next = NULL;
-    
-    if (!lst) return novo;
-    
-    else {
-        lista *aux = lst;
-        while (aux->next)
-            aux = aux->next;
-        aux->next = novo;
-    }
-    
-    return lst;
-}
+int comparacoes = 0;
 
-/* Retorna o indice de ra presente em lst, -1 caso nao encontre */
-int buscaLista_ra (lista* lst, int ra)
+/* Troca os campos ra, nota e nome do jeito mais idiota possivel */
+void swapNode (lista *node1, lista *node2)
 {
-    int i = 0;
-    lista *aux = lst;
-    while (aux) {
-        if (aux->ra == ra)
-            return i;
-        i++;
-        aux = aux->next;
-    }
-    return -1;
+	int temp;
+	char tempChar[51];
+	
+	temp = node1->ra;
+	node1->ra = node2->ra;
+	node2->ra = temp;
+	
+	temp = node1->nota;
+	node1->nota = node2->nota;
+	node2->nota = temp;	
+	
+	strcpy(tempChar, node1->nome);
+	strcpy(node1->nome, node2->nome);
+	strcpy(node2->nome, tempChar);
 }
 
-/* Remove o no que contem ra */
-lista *removeList (lista *lst, int ra)
-{
-    lista *aux = lst;
-    if (aux->ra == ra) {
-        free(aux);
-        return lst->next;
-    }
-    while (aux->next) {
-        if (aux->next->ra == ra) {
-            lista *tmp = aux->next;
-            
-            if (!aux->next->next)
-                aux->next = NULL;
-            
-            else
-                aux->next = aux->next->next;
-            
-            free(tmp);
-            return lst;
-        }
-        aux = aux->next;
-    }
-    return lst;
+/* Compara e retorna, similar a strcmp, mas funciona para nos e recebe chave */
+int comparar(lista *x, lista *y, int chave) {
+    if (chave == 1)
+		return x->ra-y->ra;
+    else
+		return strcmp(x->nome,y->nome);
 }
 
-void printList(lista *lst)
-{
-    printf("LISTA:\n");
-    lista *aux = lst;
-    while (aux) {
-        printf("[%d %s %d]\n", aux->ra, aux->nome, aux->nota);
-        aux = aux->next;
-    }
-}
-
-/* Libera lst e retorna NULL */
-lista* liberar (lista *lst){
-    if (lst) {
-        lst->next = liberar(lst->next);
-        free(lst);
-    }
-    return NULL;
-}
-
-/* Retorna o menor no da lista lst, baseado em param */
-/* param: 1 = ra, 2 = nome */
-lista *minList (lista *lst, int param, int *comparacoes)
+/* Retorna o menor no da lista lst, baseado na chave, iterativo */
+lista *minList (lista *lst, int chave)
 {
     if (!lst) return NULL;
     
     lista *aux = lst;
     lista *min = aux;
     
-    while (aux->next) {
-        (*comparacoes)++;
-        if ((param == 1 && aux->ra < min->ra) ||                //MINIMO POR RA
-            (param == 2 && strcmp(aux->nome, min->nome) < 0))   //MINIMO POR NOME
+    while (aux) {
+        if (aux->next) comparacoes++;
+        if ((chave == 1 && aux->ra < min->ra)	                //MINIMO POR RA
+         || (chave == 2 && strcmp(aux->nome, min->nome) < 0))   //MINIMO POR NOME
                 min = aux;
         aux = aux->next;
     }
     return min;
 }
 
-/* ta pronta ESTA MERDA */
-/* Recebe uma lista a ser ordenada, um parametro e o ponteiro para comparacoes */
-/* Retorna a lista ordenada */
-/* param: 1 = ra, 2 = nome */
-lista *selection_sort (lista *lst, int param, int *comparacoes)
-{
-    lista *sorted = NULL;
-    lista *min = NULL;
-    
-    while (lst) {
-        min = minList(lst, param, comparacoes);
-        sorted = appendList(sorted, min->nome, min->ra, min->nota);
-        lst = removeList(lst, min->ra);
+/* Recursivo, na main: lst = menor = lst */
+lista *min (lista *lst, lista *menor, int chave) {
+    if (lst == NULL)
+        return menor;
+	
+    if (chave == 1) {
+        if(lst->ra < menor->ra)
+            return min(lst->next, lst, chave);
+        else
+            return min(lst->next, menor, chave);
     }
-    liberar(lst);
-    return sorted;
+	
+    if (chave == 2) {
+        if(strcmp(lst->nome, menor->nome) < 0)
+            return min(lst->next, lst, chave);
+        else
+            return min(lst->next, menor, chave);
+    }
 }
 
-/* Recebe a lista e retorna ordenada */
-/* Retorna a lisa ordenada, param: 1 = ra, 2 = nome */
-lista *insertion_sort(lista *lst, int param, int *comparacoes)
-{
-    //...
+/* Cria nó */
+lista *criar (int ra, char nome[], int nota) {
+    lista *novo = malloc(sizeof(lista));
+    strcpy(novo->nome,nome);
+    novo->ra = ra;
+    novo->nota = nota;
+    novo->next = NULL;
+    return novo;
 }
 
-/* Chama o devido algoritmo de ordenacao e retorna a lista ordenada */
-/* 1: selection_sort; 2: insertion_sort */
-lista *ordenar (lista *lst, int algoritmo, int param, int *comparacoes)
-{
-    if (algoritmo == 1)
-        lst = selection_sort(lst, param, comparacoes);
-    
-    else if (algoritmo == 2)
-        lst = insertion_sort(lst, param, comparacoes);
-    
+/* Add (append) recursivo */
+lista *add(lista *x, char nome[], int ra, int nota){  
+    if (x == NULL)
+		return criar(ra, nome, nota);
+	
+    x->next = add(x->next, nome, ra, nota);
+	 
+    return x;
+}
+
+/* Busca Recursivo */
+int busca(lista *x, int ra, int C){
+    if (x == NULL)
+		return -1;
+	
+    if (x->ra == ra)
+		return C;
+	
+    return busca(x->next, ra, ++C);
+}
+
+/* Imprime recursivo */
+void imprimir (lista *x) {
+    if (x != NULL) {
+        printf("[%d %s %d]\n", x->ra, x->nome, x->nota);
+        imprimir(x->next);
+    }        
+}
+
+/* Libera e retorna NULL, recursivo */
+lista *liberar (lista *x) {
+    if (x == NULL)
+		return NULL;
+	
+    if (x->next != NULL)
+		x->next = liberar(x->next);
+	
+    free(x);
+    return NULL;
+}
+
+/* chave: 1 = ra 2 = nome */
+lista *selection_sort (lista *lst, int chave){
+    lista *aux = lst;
+    lista *minimo = NULL;
+	
+    while (aux) {
+        minimo = minList(aux, chave);
+        swapNode(aux, minimo);
+        aux = aux->next;
+    }
     return lst;
 }
 
-int main ()
-{
-    char input, *nome;
-    int ra = 0, nota = 0, algoritmo = 0, parametro = 0, comparacoes = 0, busca = 0;
-    lista *lst = NULL;
-    
-    scanf("%c\n", &input);
-    
-    while (input != 'P' && input != 'p') {
-        switch(input) {
+lista *inserir(lista *tmp, lista *head, int chave){
+    if(head->next == NULL) return head;
+    if(head->next->ra != tmp->ra) head->next = inserir(tmp, head->next, chave);
+    int k = comparar(head, tmp, chave);
+    if(k > 0){
+        comparacoes++;
+        return head;
+    }
+    else{
+        tmp->next = head;
+        return tmp;
+    }
+}
+
+lista *insertion_sort(lista *head, lista *x, int chave){
+    if(x == NULL) return NULL;
+    lista *tmp = criar(x->ra, x->nome, x->nota);
+    head = inserir(tmp,head,chave);
+    comparacoes++;
+    x = x->next;
+    x->next = insertion_sort(head, x->next, chave);
+    return x;
+}
+
+/* Chama o algoritmo de ordenacao adequado */
+lista *ordenar (lista *x, int algoritmo, int chave) {
+    if (algoritmo == 1)
+		return selection_sort(x, chave);
+	
+    if (algoritmo == 2)
+		return insertion_sort(x, x, chave);
+}
+
+int main() {
+	char input, nome[51];
+    int ra, nota, algoritmo, chave;
+    lista *head = NULL;
+    while (scanf("\n%c", &input), input != 'P' && input != 'p') {
+        switch (input) {
             case 'I':
             case 'i':
                 scanf("%d %s %d", &ra, nome, &nota);
-                lst = appendList(lst, nome, ra, nota);
+                head = add(head, nome, ra, nota);
                 break;
-            
+			
             case 'O':
             case 'o':
-                scanf("%d %d", &algoritmo, &parametro);
-                lst = ordenar(lst, algoritmo, parametro, &comparacoes);
+                comparacoes = 0;
+                scanf("%d %d", &algoritmo, &chave);
+                head = ordenar(head, algoritmo, chave);
                 printf("Comparacoes=%d\n", comparacoes);
                 break;
-            
+			
             case 'B':
             case 'b':
                 scanf("%d", &ra);
-                busca = buscaLista_ra(lst, ra);
-                printf("Pos=%d\n");
+                printf("Pos=%d\n", busca(head, ra, 0));
                 break;
-            
+			
             case 'M':
             case 'm':
-                printList(lst);
+                imprimir(head);
                 break;
         }
-        scanf("\n%c", &input);
     }
-    
-    
-    liberar(lst);
-    
+    head = liberar(head);
+	
     return 0;
 }
+
+
+
+
+
+
+
+
